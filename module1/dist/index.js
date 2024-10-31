bcd5c3c7a579ed2a1def097ce19e9caae2018ee9
rafizul896 <rafizulislam896@gmail.com>
2024-10-31 22:10:51 +0600

    learn union types & Type alias

--- module1/dist/index.js
+++ module1/dist/index.js
@@ -1,50 +1,68 @@
 "use strict";
-let friends = ['a', 'b'];
-const num = [1, 2];
-let arrName = [50, 'dd'];
-// Reference Type => Object
-const Info = {
-    firstName: 'Rafizul',
-    lastName: 'Islam',
-    Roll: 778452,
-    isSingle: true
-};
-// Learning function
-function add(num1, num2) {
-    return num1 + num2;
-}
-;
-const addArrow = (num1, num2) => num1 + num2;
-// Object function method
-const bankInfo = {
-    name: 'Rafiz',
-    balance: 0,
-    addBalance(balance) {
-        return `My new balance is : ${this.balance + balance}`;
-    }
-};
-const arr = [1, 3, 5];
-const newArr = arr.map((elem) => elem * elem);
-// Spread operator 
-const fruits1 = ['apple', 'banana', 'cherry'];
-const fruits2 = ['lemon', 'mango'];
-fruits1.push(...fruits2); // OR
-const fruits = [...fruits1, ...fruits2];
-// rest operator
-const greetFriends = (...friends) => {
-    friends.map(fri => ('Hi ' + fri));
-};
-greetFriends('Alon', 'Blon', 'Clon');
-//  destructuring
-const userInfo = {
-    id: 111,
-    name: {
+{
+    let friends = ['a', 'b'];
+    const num = [1, 2];
+    let arrName = [50, 'dd'];
+    // Reference Type => Object
+    const Info = {
         firstName: 'Rafizul',
-        middleName: 'Islam',
-        lastName: 'Rafiz'
-    },
-    address: 'Mymensign'
-};
-const { id, name: { firstName, lastName }, address: from } = userInfo;
-// array destructuring
-const [a, ...rest] = fruits1;
+        lastName: 'Islam',
+        Roll: 778452,
+        isSingle: true
+    };
+    // Learning function
+    function ition(num1, num2) {
+        return num1 + num2;
+    }
+    ;
+    const addArrow = (num1, num2) => num1 + num2;
+    // Object function method
+    const bankInfo = {
+        name: 'Rafiz',
+        balance: 0,
+        addBalance(balance) {
+            return `My new balance is : ${this.balance + balance}`;
+        }
+    };
+    const arr = [1, 3, 5];
+    const newArr = arr.map((elem) => elem * elem);
+    // Spread operator 
+    const fruits1 = ['apple', 'banana', 'cherry'];
+    const fruits2 = ['lemon', 'mango'];
+    fruits1.push(...fruits2); // OR
+    const fruits = [...fruits1, ...fruits2];
+    // rest operator
+    const greetFriends = (...friends) => {
+        friends.map(fri => ('Hi ' + fri));
+    };
+    greetFriends('Alon', 'Blon', 'Clon');
+    //  destructuring
+    const userInfo = {
+        id: 111,
+        name: {
+            firstName: 'Rafizul',
+            middleName: 'Islam',
+            lastName: 'Rafiz'
+        },
+        address: 'Mymensign'
+    };
+    const { id, name: { firstName, lastName }, address: from } = userInfo;
+    // array destructuring
+    const [a, ...rest] = fruits1;
+    const student1 = {
+        name: 'Jamal',
+        roll: 11,
+        gendar: 'male'
+    };
+    const student2 = {
+        name: 'kamal',
+        roll: 12,
+        gendar: 'male'
+    };
+    const addition = (num1, num2) => num1 + num2;
+}
+{
+    const newUser = 'male';
+    const newDeveloper = 'Full Stack';
+    console.log(newDeveloper);
+}
--- module1/src/index.ts
+++ module1/src/index.ts
@@ -1,63 +1,103 @@
-let friends: string[] = ['a', 'b'];
-const num: number[] = [1, 2];
-let arrName: [number, string] = [50, 'dd'];
+{
+    let friends: string[] = ['a', 'b'];
+    const num: number[] = [1, 2];
+    let arrName: [number, string] = [50, 'dd'];
 
-// Reference Type => Object
-const Info: {
-    readonly firstName: string;
-    lastName?: string; //Optional type
-    isSingle: boolean;
-    Roll: 778452; // type --> literal
-} = {
-    firstName: 'Rafizul',
-    lastName: 'Islam',
-    Roll: 778452,
-    isSingle: true
-};
+    // Reference Type => Object
+    const Info: {
+        readonly firstName: string;
+        lastName?: string; //Optional type
+        isSingle: boolean;
+        Roll: 778452; // type --> literal
+    } = {
+        firstName: 'Rafizul',
+        lastName: 'Islam',
+        Roll: 778452,
+        isSingle: true
+    };
 
-// Learning function
-function add(num1: number, num2: number) {
-    return num1 + num2
-};
-const addArrow = (num1: number, num2: number): number => num1 + num2;
+    // Learning function
+    function ition(num1: number, num2: number) {
+        return num1 + num2
+    };
+    const addArrow = (num1: number, num2: number): number => num1 + num2;
 
-// Object function method
-const bankInfo = {
-    name: 'Rafiz',
-    balance: 0,
-    addBalance(balance: number): string {
-        return `My new balance is : ${this.balance + balance}`
+    // Object function method
+    const bankInfo = {
+        name: 'Rafiz',
+        balance: 0,
+        addBalance(balance: number): string {
+            return `My new balance is : ${this.balance + balance}`
+        }
     }
+
+    const arr: number[] = [1, 3, 5];
+    const newArr: number[] = arr.map((elem: number): number => elem * elem);
+
+    // Spread operator 
+    const fruits1: string[] = ['apple', 'banana', 'cherry'];
+    const fruits2: string[] = ['lemon', 'mango'];
+    fruits1.push(...fruits2); // OR
+    const fruits = [...fruits1, ...fruits2];
+
+    // rest operator
+    const greetFriends = (...friends: string[]) => {
+        friends.map(fri => ('Hi ' + fri))
+    };
+
+    greetFriends('Alon', 'Blon', 'Clon');
+
+    //  destructuring
+    const userInfo = {
+        id: 111,
+        name: {
+            firstName: 'Rafizul',
+            middleName: 'Islam',
+            lastName: 'Rafiz'
+        },
+        address: 'Mymensign'
+    };
+
+    const { id, name: { firstName, lastName }, address: from } = userInfo;
+
+    // array destructuring
+    const [a, ...rest] = fruits1;
+
+    // Type alias
+    type Student = {
+        name: string;
+        roll: number;
+        gendar: string;
+    }
+
+    const student1: Student = {
+        name: 'Jamal',
+        roll: 11,
+        gendar: 'male'
+    }
+
+    const student2: Student = {
+        name: 'kamal',
+        roll: 12,
+        gendar: 'male'
+    }
+
+    type Addition = (num1: number, num2: number) => number
+    const addition: Addition = (num1, num2) => num1 + num2;
+
 }
 
-const arr: number[] = [1, 3, 5];
-const newArr: number[] = arr.map((elem: number): number => elem * elem);
+{
+    // union types
+    type Gendar = 'male' | 'female';
+    const newUser: Gendar = 'male';
 
-// Spread operator 
-const fruits1: string[] = ['apple', 'banana', 'cherry'];
-const fruits2: string[] = ['lemon', 'mango'];
-fruits1.push(...fruits2); // OR
-const fruits = [...fruits1, ...fruits2];
+    type FrontendDeveloper = 'fakibazDeveloper' | 'juniorDeveloper';
+    type DeveloperStack = 'MERN Stack' | 'Full Stack';
 
-// rest operator
-const greetFriends = (...friends: string[]) => {
-    friends.map(fri => ('Hi ' + fri))
-};
+    type Developer = FrontendDeveloper | DeveloperStack;
 
-greetFriends('Alon', 'Blon', 'Clon');
+    const newDeveloper: Developer = 'Full Stack';
+    console.log(newDeveloper)
 
-//  destructuring
-const userInfo = {
-    id: 111,
-    name: {
-        firstName: 'Rafizul',
-        middleName: 'Islam',
-        lastName: 'Rafiz'
-    },
-    address: 'Mymensign'
-};
-
-const { id, name: { firstName, lastName }, address: from } = userInfo;
-
-// array destructuring
-const [a,...rest] = fruits1;
+}
