1005b1d828cf0746367bd1c828c7ab4904664dbc
rafizul896 <rafizulislam896@gmail.com>
2024-10-31 20:25:42 +0600

    learn destructuring

--- module1/dist/index.js
+++ module1/dist/index.js
@@ -3,7 +3,7 @@ let friends = ['a', 'b'];
 const num = [1, 2];
 let arrName = [50, 'dd'];
 // Reference Type => Object
-const user = {
+const Info = {
     firstName: 'Rafizul',
     lastName: 'Islam',
     Roll: 778452,
@@ -16,7 +16,7 @@ function add(num1, num2) {
 ;
 const addArrow = (num1, num2) => num1 + num2;
 // Object function method
-const bankUser = {
+const bankInfo = {
     name: 'Rafiz',
     balance: 0,
     addBalance(balance) {
@@ -28,4 +28,23 @@ const newArr = arr.map((elem) => elem * elem);
 // Spread operator 
 const fruits1 = ['apple', 'banana', 'cherry'];
 const fruits2 = ['lemon', 'mango'];
-fruits1.push(...fruits2);
+fruits1.push(...fruits2); // OR
+const fruits = [...fruits1, ...fruits2];
+// rest operator
+const greetFriends = (...friends) => {
+    friends.map(fri => ('Hi ' + fri));
+};
+greetFriends('Alon', 'Blon', 'Clon');
+//  destructuring
+const userInfo = {
+    id: 111,
+    name: {
+        firstName: 'Rafizul',
+        middleName: 'Islam',
+        lastName: 'Rafiz'
+    },
+    address: 'Mymensign'
+};
+const { id, name: { firstName, lastName }, address: from } = userInfo;
+// array destructuring
+const [a, ...rest] = fruits1;
--- module1/src/index.ts
+++ module1/src/index.ts
@@ -3,7 +3,7 @@ const num: number[] = [1, 2];
 let arrName: [number, string] = [50, 'dd'];
 
 // Reference Type => Object
-const user: {
+const Info: {
     readonly firstName: string;
     lastName?: string; //Optional type
     isSingle: boolean;
@@ -22,7 +22,7 @@ function add(num1: number, num2: number) {
 const addArrow = (num1: number, num2: number): number => num1 + num2;
 
 // Object function method
-const bankUser = {
+const bankInfo = {
     name: 'Rafiz',
     balance: 0,
     addBalance(balance: number): string {
@@ -34,9 +34,30 @@ const arr: number[] = [1, 3, 5];
 const newArr: number[] = arr.map((elem: number): number => elem * elem);
 
 // Spread operator 
-
 const fruits1: string[] = ['apple', 'banana', 'cherry'];
 const fruits2: string[] = ['lemon', 'mango'];
-fruits1.push(...fruits2);
+fruits1.push(...fruits2); // OR
+const fruits = [...fruits1, ...fruits2];
 
+// rest operator
+const greetFriends = (...friends: string[]) => {
+    friends.map(fri => ('Hi ' + fri))
+};
 
+greetFriends('Alon', 'Blon', 'Clon');
+
+//  destructuring
+const userInfo = {
+    id: 111,
+    name: {
+        firstName: 'Rafizul',
+        middleName: 'Islam',
+        lastName: 'Rafiz'
+    },
+    address: 'Mymensign'
+};
+
+const { id, name: { firstName, lastName }, address: from } = userInfo;
+
+// array destructuring
+const [a,...rest] = fruits1;
