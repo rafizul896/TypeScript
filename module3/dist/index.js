7b29cb4e2462ede8e8679bc02b3ce72ec96f499f
rafizul896 <rafizulislam896@gmail.com>
2024-11-03 21:22:45 +0600

    Inheritance in OOP

--- module3/dist/index.js
+++ module3/dist/index.js
@@ -16,3 +16,42 @@
     const cat = new Animal('Mimi', 'firend', 'meaw meaw');
     cat.makeSound();
 }
+// Inheritance in OOP
+{
+    class Person {
+        constructor(name, age, address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+        ;
+        getSleep(hours) {
+            console.log(`${this.name} will sleep for ${hours}`);
+        }
+        ;
+    }
+    class Student extends Person {
+        constructor(name, age, address) {
+            super(name, age, address);
+        }
+        getSleep(hours) {
+            console.log(`${this.name} will sleep for ${hours}`);
+        }
+    }
+    const student1 = new Student('Rafiz', 18, 'Phulpur');
+    console.log(student1);
+    student1.getSleep(8);
+    class Teacher extends Person {
+        constructor(name, age, address, designation) {
+            super(name, age, address);
+            this.designation = designation;
+        }
+        getSleep(hours) {
+            console.log(`${this.name} will sleep for ${hours}`);
+        }
+        takeClass(numOfClass) {
+            console.log(`${this.name} will take ${numOfClass}`);
+        }
+    }
+    const teacher1 = new Teacher('Mr. Teacher', 38, 'Phulpur', 'Professor');
+}
--- module3/src/index.ts
+++ module3/src/index.ts
@@ -14,3 +14,52 @@
     cat.makeSound();
 
 }
+
+// Inheritance in OOP
+{
+    class Person {
+        name: string;
+        age: number;
+        address: string;
+
+        constructor(name: string, age: number, address: string) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        };
+        getSleep(hours: number) {
+            console.log(`${this.name} will sleep for ${hours}`)
+        };
+    }
+
+    class Student extends Person {
+        constructor(name: string, age: number, address: string) {
+            super(name, age, address);
+        }
+        getSleep(hours: number) {
+            console.log(`${this.name} will sleep for ${hours}`)
+        }
+    }
+
+    const student1 = new Student('Rafiz', 18, 'Phulpur')
+    console.log(student1);
+    student1.getSleep(8);
+
+    class Teacher extends Person {
+        designation: string;
+
+        constructor(name: string, age: number, address: string, designation: string) {
+            super(name, age, address);
+            this.designation = designation;
+        }
+        getSleep(hours: number) {
+            console.log(`${this.name} will sleep for ${hours}`)
+        }
+        takeClass(numOfClass: number) {
+            console.log(`${this.name} will take ${numOfClass}`)
+        }
+    }
+
+    const teacher1 = new Teacher('Mr. Teacher', 38, 'Phulpur', 'Professor');
+}
+
